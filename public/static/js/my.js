51ca72be7660e33edc09c348945c557c0b791458
1414641776@qq.com <473599201@qq.com>
2016-09-26 09:43:58 +0800

    static commit

---
+++ public/static/js/my.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	if (n==1) {
+	$("#menu_wrap").removeClass("nav-bg").addClass("nav-bgn");
+	}
+	else {
+		$("#zt").removeClass("container m0 bod top70").addClass("container-fluid m");
+	}
+});
